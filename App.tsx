adc6526bb4dea91d3ee0e6c1acd4e6ae56017f33
Benjamin Gamrekeli <benjamin.gamrekeli@student.ap.be>
2024-12-01 16:41:45 +0100

    initial setup with routing

--- App.tsx
+++ App.tsx
@@ -1,10 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Outlet, createBrowserRouter, RouterProvider, Route, NavLink } from "react-router-dom";
+import State from "./types"
+
+const Root = () => {
+  return (
+      <View>
+        <View>
+                <NavLink to="/" >Home</NavLink>
+                <NavLink to="page1">Page 1</NavLink>
+                <NavLink to="page2">Page 2</NavLink>
+            </View>
+            <View>
+                <Outlet/>
+            </View>
+      </View>
+  );
+}
+
+const Home = () => {
+  return (
+      <View>This is the home page!</View>
+  );
+}
+
+const Page1 = () => {
+  return (
+      <View>Page 1</View>
+  );
+}
+
+const Page2 = () => {
+  return (
+      <View>Page 2</View>
+  );
+}
 
 export default function App() {
+  const [states, setStates] = useState<State[]>();
+  useEffect(()=> {
+    const loadData = async() => {
+      const data = await fetch("https://sampleapis.assimilate.be/thestates/states");
+      const json = await data.json();
+      setStates(json);
+    }
+    loadData();
+  }, []);
+
+  const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        children: [
+            {
+                path: "",
+                element: <Home/>
+            },
+            {
+                path: "page1",
+                element: <Page1/>
+            },
+            {
+                path: "page2",
+                element: <Page2/>
+            }
+        ]
+    }
+]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <RouterProvider router={router}/>
+      <Text>Open up App.tsx to  on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ types.ts
@@ -0,0 +1,10 @@
+export default interface State {
+    id:number,
+    name:string,
+    abv:string,
+    capital:string,
+    largest_city:string,
+    admitted_to_union:string,
+    population:string,
+    flag:string
+}
